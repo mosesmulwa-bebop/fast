120747f120b76a2ff2013dd80b29cb096b73c61f
Moses Mulwa <mosesmulwa123@gmail.com>
2022-03-26 00:23:39 +0300

    added error checking

--- app/routers/convert.py
+++ app/routers/convert.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,status, HTTPException
 import requests
 
 from ..utils import make_convert_url
@@ -10,17 +10,21 @@ router  = APIRouter(
 @router.get('/convert/')
 def convert(from_curr:str, to_curr:str, value_to_convert):
     final_api_url = make_convert_url(from_curr,to_curr)
-    response = requests.get(final_api_url).json()
-    key_dict = from_curr+"_"+to_curr
-    rate = response[key_dict]
-    value_to_convert = float(value_to_convert)
-    answer = rate * value_to_convert
-    my_dict = {
-        "From":from_curr,
-        "To":to_curr,
-        "Value" : value_to_convert,
-        "Rate" :rate,
-        "Converted": answer
-    }
-    #return answer
-    return my_dict
+    try:
+        response = requests.get(final_api_url).json()
+        key_dict = from_curr+"_"+to_curr
+        rate = response[key_dict]
+        value_to_convert = float(value_to_convert)
+        answer = rate * value_to_convert
+        my_dict = {
+            "From":from_curr,
+            "To":to_curr,
+            "Value" : value_to_convert,
+            "Rate" :rate,
+            "Converted": answer
+        }
+        #return answer
+        return my_dict
+    except:
+        raise HTTPException(status_code=status.HTTP_502_BAD_GATEWAY, 
+                            detail=f"API Call to Currency Converter API failed")
